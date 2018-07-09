fe6d4059326b77973cd8a2fb0bf282672cc59f2d
johnniereg <johnniereg@gmail.com>
2018-07-08 19:41:13 -0700

    Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = require('../routes/routes.js')
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+// @TODO: This password shouldn't be hard written in here...
+mongoose.connect('mongodb://scientist:feed100ducks@ds131551.mlab.com:31551/ifedaduck');
 
 app.use('/', router);
 
